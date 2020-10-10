2607f2416b1fb67c609008c6c7e27797966fd819
ankitsejwal <ankit@sejw.al>
2020-10-10 22:32:22 +1100

    implemented clock

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+const local = document.getElementById('local-time');
+
+function clock() {
+    // get todays date
+    const date = new Date();
+    const localTime = date.getTime();
+    const localOffset = date.getTimezoneOffset() * 60000; // millisecond
+    const utc = localTime + localOffset; // time in utc
+
+    const destinationOffset = -7; // pdt
+    let destinationTime = utc + destinationOffset * 3600000;
+    destinationTime = new Date(destinationTime);
+
+    console.log('local time: ' + localTime);
+    console.log('Timezone Offset: ' + localOffset);
+    console.log('utc: ' + utc);
+    console.log(destinationTime.toLocaleString());
+
+    document.getElementById('local-time').innerHTML = date.toLocaleString();
+    document.getElementById(
+        'dest-time'
+    ).innerHTML = destinationTime.toLocaleString();
+}
+
+function counter() {
+    const days = document.getElementById('days');
+    const minutes = document.getElementById('minutes');
+    const hours = document.getElementById('hours');
+    const seconds = document.getElementById('seconds');
+    const date = new Date();
+
+    hours.innerHTML = date.getHours();
+    minutes.innerHTML = date.getMinutes();
+    seconds.innerHTML = date.getSeconds();
+
+    setTimeout(function () {
+        clock();
+        counter();
+    }, 1000);
+}
+
+counter();
